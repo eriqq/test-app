d3ae15b694506f121c174bb8b2e4a2cddabaa16e
eriqq <erik.bjerkfeldt@gmail.com>
2018-10-30 15:54:27 +0100

    Updates

---
+++ precache-manifest.83777a66dfae25b98959c2046528d2e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3510dee076921078b779",
+    "url": "/test-app/static/js/runtime~main.3510dee0.js"
+  },
+  {
+    "revision": "ee270a46d68f23909376",
+    "url": "/test-app/static/js/main.ee270a46.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/test-app/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "ee270a46d68f23909376",
+    "url": "/test-app/static/css/main.a97e2314.chunk.css"
+  },
+  {
+    "revision": "271c6642e4320de2ba93dec00558ae73",
+    "url": "/test-app/index.html"
+  }
+];
